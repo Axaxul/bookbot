2ed75b34f097c5123dffce2dcf130d8312794541
Axaxul <sissyfish182@gmail.com>
2025-07-09 23:18:00 -0400

    changed readout to wordcount

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-def get_book_text(file_path):
+def main():
+    file_path = "books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
+    num_words = word_count(file_contents)
+    print (f"{num_words} words found in the document")
 
+def get_book_text(file_path):
     with open(file_path) as f:
         book = f.read()
     return (book)
 
-
-def main():
-
-    file_path = "books/frankenstein.txt"
-    file_contents = get_book_text(file_path)
-
-
-    print(file_contents)
+def word_count(file_contents):
+    words_length = len(file_contents.split())
+    return words_length
 
 main()
