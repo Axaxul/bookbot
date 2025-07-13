894d8b5437407517e5bca262de1511130ffcf6cc
Axaxul <sissyfish182@gmail.com>
2025-07-12 23:11:33 -0400

    Bookbot project print report

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 def main():
+    #list of Definitions
     file_path = "books/frankenstein.txt"
     file_contents = get_book_text(file_path)
     num_words = word_count(file_contents)
-    print (f"{num_words} words found in the document")
-    symbols = symbol_counter(file_contents)
-    print (f" This is the result of test: {symbols}")
+    individual_symbols = symbol_counter(file_contents)
+
+    sorted_final = organized_list(individual_symbols,)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print (f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_final:
+        print(f"{char_dict['char']}: {char_dict['num']}")
+
+    print("============= END ===============")
 
 def get_book_text(file_path):
     with open(file_path) as f:
         book = f.read()
     return (book)
 
-from stats import word_count, symbol_counter
+from stats import word_count, symbol_counter, organized_list
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,25 @@ def symbol_counter(file_contents):
         # We then add 1 to this value and update the dictionary.
 
     return individual_symbols
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def organized_list(individual_symbols):
+        char_list = [{"char": key, "num": value} for key, value in individual_symbols.items() if key.isalpha()]
+        char_list.sort(reverse=True, key=sort_on)
+
+        return char_list
+
+
+#Add a new function to your stats.py file that takes the dictionary of
+# characters and their counts and returns a sorted list of dictionaries.
+#Each dictionary should have two key-value pairs: one for the character
+# itself and one for that character's count (e.g. {"char": "b", "num": 4868}).
+#The .sort() method will be helpful here (see the example).
+#Import and call the function in main.py, and capture the result.
+#Print the report to the console as shown above.
+#  If the character is not an alphabetical character (using the .isalpha() method),
+#  just skip it.
